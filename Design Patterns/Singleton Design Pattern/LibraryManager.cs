530bd47147c1336bfab109092a0ccf860d307e2d
BinodPerera <108719707+BinodPerera@users.noreply.github.com>
2024-09-25 21:41:04 +0530

    Singleton Design Pattern

---
+++ Design Patterns/Singleton Design Pattern/LibraryManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Singleton_Design_Pattern
+{
+	public class LibraryManager
+	{
+		public static LibraryManager instance;	// creating instance variable
+
+		private LibraryManager()	// This is constructor when creating an object for this class this body will auto run
+		{
+			Console.WriteLine("instance created!");	// This will print after creating an object from this class
+		}
+
+		public static LibraryManager getInstance()	// This getInstance() method is use for creating object for this class 
+		{
+			if(instance == null)	// checking this instance variable is null or not
+			{
+				//	If this instance variable is null it will assign new value to this instance variable
+				instance = new LibraryManager();
+			}
+			return instance;	// returning instance variable value
+		}
+
+		public void CreateUser()	// method for creating user account
+		{
+			Console.WriteLine("User Account Created!");
+		}
+
+		public void AddBook()	// method for add book into library	
+		{
+			Console.WriteLine("Book added!");
+		}
+	}
+
+}
+
---
+++ Design Patterns/Singleton Design Pattern/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Singleton_Design_Pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LibraryManager manager = LibraryManager.getInstance(); // creating object using instance 
+            manager.AddBook();  // calling methods inside class
+            manager.CreateUser();
+        }
+    }
+}
